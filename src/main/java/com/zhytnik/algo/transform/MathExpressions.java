59bf02386298140138283f31b1aae327780ac0de
Alexey Zhytnik <alexeyzh@playtika.com>
2021-07-19 22:39:06 +0300

    acceleration

--- src/main/java/com/zhytnik/algo/transform/MathExpressions.java
+++ src/main/java/com/zhytnik/algo/transform/MathExpressions.java
@@ -4,10 +4,10 @@ import com.zhytnik.algo.math.BinaryOperation;
 import com.zhytnik.algo.math.BinaryOperator;
 import com.zhytnik.algo.math.Expression;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class MathExpressions implements Transformation {
+public final class MathExpressions implements Transformation {
 
     private static final int[] EXPECTED_SIZE = {0, 0, 4, 32, 320, 3584, 43_008, 540_672, 7_028_736};
 
@@ -25,46 +25,50 @@ public class MathExpressions implements Transformation {
         if (count < 2 || count > 8) {
             throw new IllegalArgumentException("Source size should have value from [2, 8] range! Actual is " + count);
         }
-        return expressions(source, 0, count);
+        return Arrays.asList(expressions(source, 0, count));
     }
 
-    private List<Expression> expressions(List<Expression> source, int from, int to) {
+    private static Expression[] expressions(List<Expression> source, int from, int to) {
         int total = to - from;
 
         if (total == 1) {
-            return List.of(source.get(from));
+            return new Expression[]{source.get(from)};
         } else if (total == 2) {
             return allBiCombinations(source.get(from), source.get(from + 1));
         }
 
-        List<Expression> result = new ArrayList<>(EXPECTED_SIZE[total]);
+        Expression[] result = new Expression[EXPECTED_SIZE[total]];
 
-        for (int i = 1; i < total; i++) {
+        for (int i = 1, offset = 0; i < total; i++) {
             var left = expressions(source, from, from + i);
             var right = expressions(source, from + i, to);
 
-            fillByAllBiCombinations(result, left, right);
+            fillByAllBiCombinations(result, offset, left, right);
+            offset += BINARY_OPERATORS.length * left.length * right.length;
         }
         return result;
     }
 
-    private List<Expression> allBiCombinations(Expression left,
-                                               Expression right) {
-        List<Expression> result = new ArrayList<>(BINARY_OPERATORS.length);
-
-        for (var op : BINARY_OPERATORS) {
-            result.add(new BinaryOperation(left, right, op));
-        }
-        return result;
+    private static Expression[] allBiCombinations(Expression left, Expression right) {
+        return new Expression[]{
+                new BinaryOperation(left, right, BinaryOperator.DIVISION),
+                new BinaryOperation(left, right, BinaryOperator.ADDITION),
+                new BinaryOperation(left, right, BinaryOperator.SUBTRACTION),
+                new BinaryOperation(left, right, BinaryOperator.MULTIPLICATION)
+        };
     }
 
-    private void fillByAllBiCombinations(List<Expression> target,
-                                         List<Expression> lefts,
-                                         List<Expression> rights) {
-        for (var op : BINARY_OPERATORS) {
-            for (var left : lefts) {
-                for (var right : rights) {
-                    target.add(new BinaryOperation(left, right, op));
+    private static void fillByAllBiCombinations(Expression[] target, int offset, Expression[] lefts, Expression[] rights) {
+        int leftSize = lefts.length, rightSize = rights.length;
+
+        for (int k = 0, ops = BINARY_OPERATORS.length; k < ops; k++) {
+            var op = BINARY_OPERATORS[k];
+
+            for (int i = 0; i < leftSize; i++) {
+                var left = lefts[i];
+
+                for (int j = 0; j < rightSize; j++) {
+                    target[offset + (k * leftSize * rightSize) + (i * rightSize) + j] = new BinaryOperation(left, rights[j], op);
                 }
             }
         }
